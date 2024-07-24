04f7adf44362c7a27782afdd75687b57c990ebe0
Tinghui Wu <tinghui.ntu@gmail.com>
2024-07-23 19:29:20 -0700

    modify histology image cropping script

--- hist_crop.py
+++ hist_crop.py
@@ -1,12 +1,13 @@
 import os
 from glob import glob
+from collections import defaultdict
 import numpy as np
+import pandas as pd
 import cv2
 from matplotlib import pyplot as plt
 import matplotlib.patches as patches
 import random
 from tqdm import tqdm
-import time
 
 
 def main():
@@ -16,86 +17,76 @@ def main():
     save_dir = os.path.join(root_dir, 'patch_images')
 
     # Set up parameters
-    output_size = 1000
-    shrink_ratio = 10
-    shrink_size = output_size // shrink_ratio
-    crops_per_image = 10
-    time_limit = 180
+    output_size = 1000 # desired output image size
+    n_patch_per_image = 10 # desired number of output patches per image
+    patch_overlap_rate = 0.5 # cropped patch overlap rate
+    shrink_ratio = 10 # for faster operation
+
+    shrink_patch_size = output_size // shrink_ratio
+    shrink_patch_shift = int(np.floor(shrink_patch_size * patch_overlap_rate))
 
     fail_case = []
+    rows = []
 
     for date in date_list:
+        path_dict = defaultdict(int)
         print(date)
         if not os.path.isdir(os.path.join(save_dir, date)):
             os.mkdir(os.path.join(save_dir, date))
 
         img_list = glob(os.path.join(root_dir, date, '*', '*', '*.tif'))
 
-        for img_path in tqdm(img_list):
-            img_ID = img_path.split('/')[-1].split('.')[0]
-            image = cv2.imread(img_path, cv2.COLOR_BGR2RGB)
+        for img_path in img_list:
+        # for img_path in tqdm(img_list):
+            case_id = img_path.split('/')[-3]
+            child_path = '/'.join(img_path.split('/')[-2:])
+            path_dict[case_id] += 1
+
+            image = cv2.imread(img_path)
 
             # Step 1: downsample image
             image_downsampled = downsample(image, shrink_ratio)
-            h, w = image_downsampled.shape[:2]
+            ori_image_downsampled = cv2.cvtColor(image_downsampled, cv2.COLOR_BGR2RGB)
+            image_downsampled = cv2.cvtColor(image_downsampled, cv2.COLOR_BGR2HSV)
 
-            # Step 2: flood fill to turn the background white
-            fill_mask = np.zeros((h+2, w+2), np.uint8)
-            cv2.floodFill(image_downsampled, fill_mask, (0, 0), (225, 225, 225))
-            cv2.floodFill(image_downsampled, fill_mask, (w-1, 0), (225, 225, 225))
-            cv2.floodFill(image_downsampled, fill_mask, (0, h//2), (225, 225, 225))
-            cv2.floodFill(image_downsampled, fill_mask, (0, h-1), (225, 225, 225))
-            cv2.floodFill(image_downsampled, fill_mask, (w-1, h-1), (225, 225, 225))
-            ori_image_downsampled = image_downsampled.copy()
+            height, width, _ = np.shape(image_downsampled)
 
-            # Step 3: block out the upper left and lower left corner
-            crop_w = w // 4
-            crop_h = h // 5
-            image_downsampled[:crop_h, :crop_w, :] = (225, 225, 225)
-            image_downsampled[-crop_h:, :crop_w, :] = (225, 225, 225) 
-
-            # Step 4: reverse the image
-            image_downsampled = cv2.bitwise_not(image_downsampled)
-
-            # Step 5: generate 3*planning boxes
+            # Step 2: generate blue area mask
+            lower_blue = np.array([100, 50, 50])
+            upper_blue = np.array([140, 255, 255])
+            mask = cv2.inRange(image_downsampled, lower_blue, 
+                               upper_blue)
+            
             box_idx = []
+            thr = 0.1
+            if date == 'AI_P150':
+                thr = 0.05
 
-            lower_color = np.array([125, 150, 150])
-            upper_color = np.array([175, 225, 225])
-            color_mask = cv2.inRange(image_downsampled, lower_color, upper_color)
-            kernel = np.ones((3, 3), np.uint8)
-            color_mask = cv2.erode(color_mask, kernel, iterations=1)
-            color_mask = cv2.dilate(color_mask, kernel, iterations=2)
+            # Step 3: crop patches
+            max_thr = 0
+            for i in range(height // shrink_patch_shift):
+                for j in range(width // shrink_patch_shift):
+                    bottom = min(i*shrink_patch_shift + shrink_patch_size, height)
+                    right = min(j*shrink_patch_shift + shrink_patch_size, width)
+                    top = bottom - shrink_patch_size
+                    left = right - shrink_patch_size
 
-            mid_h = random.randint(0, h)
-            mid_w = random.randint(0, w)
-            top, bottom, left, right = mid_h - shrink_size//2, mid_h + shrink_size//2, mid_w - shrink_size//2, mid_w + shrink_size//2
-
-            start_time = time.time()
-            while len(box_idx)<crops_per_image*3:
-                if time.time() - start_time > time_limit:
-                    print(f"Couldn't find patches in {img_ID}")
-                    break
-                if top >=0 and bottom <= h and left >=0 and right<=w:
-                    patch = image_downsampled[top:bottom, left:right]
-                    mask_patch = color_mask[top:bottom, left:right]
-                    
-                    if (mask_patch.sum() / (shrink_size*shrink_size*225) > 0.2) and ((patch < 10).sum() < 10):
-                        box_idx.append([top, bottom, left, right, mask_patch.sum() / (shrink_size*shrink_size*225)])
-
-                mid_h = random.randint(0, h)
-                mid_w = random.randint(0, w)
-                top, bottom, left, right = mid_h - shrink_size//2, mid_h + shrink_size//2, mid_w - shrink_size//2, mid_w + shrink_size//2
-
+                    patch = mask[top:bottom, left:right]
+                    this_thr = np.sum(patch > 0) / (shrink_patch_size*shrink_patch_size)
+                    max_thr = max(max_thr, this_thr)
+                    if this_thr > thr:
+                        box_idx.append([top, bottom, left, right, this_thr])
+            ori_n_box = len(box_idx)
             if len(box_idx) == 0:
-                fail_case.append(f"{date}_{img_ID}")
+                fail_case.append((f"{date}_{case_id}", max_thr))
                 continue
+            
+            # Step 4: remove overlapping patches
+            box_idx = remove_overlapping_boxes(box_idx, 0.1)
+            rm_overlap_n_box = len(box_idx)
 
-            # Step 6: remove overlapping boxes
-            box_idx = remove_overlapping_boxes(box_idx, 0.2)
-            if len(box_idx) > crops_per_image:
-                box_idx.sort(key=lambda x: x[-1], reverse=True)
-                box_idx = box_idx[:crops_per_image]
+            if len(box_idx) > n_patch_per_image:
+                box_idx = random.sample(box_idx, n_patch_per_image)
 
             fig, ax = plt.subplots()
             plt.margins(0, 0)
@@ -107,13 +98,21 @@ def main():
                 rect = patches.Rectangle((left, top), width, height, linewidth=1, edgecolor='r', facecolor='none')
                 ax.add_patch(rect)
             plt.axis('off')
-            plt.savefig(os.path.join(save_dir, date, f'map_{img_ID}.png'))
+            plt.savefig(os.path.join(save_dir, date, f'map_{case_id}_{path_dict[case_id]}.png'))
             plt.close()
 
             for idx, square in enumerate(box_idx):
                 top, bottom, left, right, _ = square
                 crop_img = image[top*shrink_ratio:bottom*shrink_ratio, left*shrink_ratio:right*shrink_ratio]
-                cv2.imwrite(os.path.join(save_dir, date, f"{img_ID}_{idx}.png"), crop_img)
+                cv2.imwrite(os.path.join(save_dir, date, f"{case_id}_{path_dict[case_id]}_{idx}.png"), crop_img)
+
+            print(f'Date: {date}, {case_id}-{path_dict[case_id]} got {ori_n_box}->{rm_overlap_n_box}->{len(box_idx)} images')
+
+            new_row = {'date': date, 'case_id': case_id, 'ori_img_path': child_path, 'given_id': path_dict[case_id]}
+            rows.append(new_row)
+
+    df = pd.DataFrame(rows)
+    df.to_csv(os.path.join(save_dir, f'save_path.csv'), index=False)
 
     print(fail_case)
     
@@ -126,6 +125,7 @@ def downsample(mask, factor):
     downsampled_mask = cv2.resize(mask, (new_width, new_height), interpolation=cv2.INTER_NEAREST)
     
     return downsampled_mask
+
 
 def calculate_overlap_area(box1, box2):
     # box1 and box2 are tuples or lists containing (x1, y1, x2, y2) coordinates
@@ -150,26 +150,33 @@ def remove_overlapping_boxes(boxes, overlap_threshold):
     box_area = calculate_overlap_area(boxes[0], boxes[0])
     
     for i, box1 in enumerate(boxes):
-        if i in removed_indices:
+        if i in removed_indices or box1 in boxes_to_keep:
             continue
         
+        curr_candidate = (i, box1.copy())
         for j in range(i + 1, len(boxes)):
             if j in removed_indices:
                 continue
             
             box2 = boxes[j]
+            if box2 in boxes_to_keep:
+                continue
+
             # Calculate overlap area between box1 and box2
-            overlap_area = calculate_overlap_area(box1, box2)
+            overlap_area = calculate_overlap_area(curr_candidate[1], box2)
             
             # Calculate overlap ratio
             overlap_ratio = overlap_area / (box_area*2 - overlap_area)
             
             # Check if overlap ratio exceeds threshold
             if overlap_ratio > overlap_threshold:
-                removed_indices.add(j)
-        
-        if i not in removed_indices:
-            boxes_to_keep.append(box1)
+                if curr_candidate[1][4] > box2[4]:
+                    removed_indices.add(j)
+                else:
+                    removed_indices.add(curr_candidate[0])
+                    curr_candidate = (j, box2.copy())
+                    
+        boxes_to_keep.append(curr_candidate[1])
     
     return boxes_to_keep
 
